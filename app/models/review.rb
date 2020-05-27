8113e6a1ae85e9c3766b4326aff12218a8d16a9f
Marvin Carrasco <marccenvivo@gmail.com>
2020-05-27 00:35:59 -0500

    add callbacks in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,15 @@ class Review < ApplicationRecord
   belongs_to :user
 
   belongs_to :reviewable, polymorphic: true
+
+  #callback
+  after_create do |review|
+    user = review.user
+    user.update(review_count: user.review_count + 1)
+  end
+
+  after_destroy do |review|
+    user = review.user
+    user.update(review_count: user.review_count - 1)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :reviews
+
+  #callback
+  before_create do |user|
+    user.review_count = 0
+  end
 end
