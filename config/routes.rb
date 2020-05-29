2931e04a989c527c4b666e6ba22333fced440648
Marvin Carrasco <marccenvivo@gmail.com>
2020-05-29 09:08:26 -0500

    destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   
   #get 'games', to: 'games#index'
   #get 'games/:id', to: 'games#show'
-  resources :games, only: [:index, :show, :new, :create, :edit, :update]
+  resources :games, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   #get 'companies', to: 'companies#index'
   #get 'companies/:id', to: 'companies#show'
-  resources :companies, only: [:index, :show, :new, :create, :edit, :update]
+  resources :companies, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
