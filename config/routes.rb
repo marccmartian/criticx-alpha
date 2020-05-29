40a82b43c4e0674bc6fc1eb59618f91c47df7b70
Marvin Carrasco <marccenvivo@gmail.com>
2020-05-28 23:34:44 -0500

    add new action to create new views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #resources :games
-  get 'games', to: 'games#index'
-  get 'games/:id', to: 'games#show'
+  
+  #get 'games', to: 'games#index'
+  #get 'games/:id', to: 'games#show'
+  resources :games, only: [:index, :show, :new, :create, :edit, :update]
 
-  get 'companies', to: 'companies#index'
-  get 'companies/:id', to: 'companies#show'
+  #get 'companies', to: 'companies#index'
+  #get 'companies/:id', to: 'companies#show'
+  resources :companies, only: [:index, :show, :new, :create, :edit, :update]
 end
